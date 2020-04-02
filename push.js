a9bc7b72d63ebc6f15938a3ce0ed4dcfabf5b844
Quinton0121 <quinton0121@gmail.com>
2020-04-02 02:16:15 -0700

    fix

--- push.js
+++ push.js
@@ -7,5 +7,5 @@ let vapidKey = { publicKey:
 push.setVapidDetails('mialto:test@code.co.uk',vapidKeys.publicKey,vapidKeys.privateKey)
 
 
-let sub={};
+let sub={"endpoint":"https://fcm.googleapis.com/fcm/send/dU8My47N8S4:APA91bEQ_dzOQiVnF9VbcO9qLYIBTCvTesoHGJ93rJVGROhioPHHCOQATHtsohyz1MOys4AtJFmJN0NE0OMSZUn5D81_mDPJlAL-UpNwmFkzprLc6jkc0-I6fLEqmtbAn_LYta0tmiFo","expirationTime":null,"keys":{"p256dh":"BHe-Cz6QrRf2KXvw639lSWJjAD1E8VxROCSugNaXAKUlVUYmxs4di-VmgUBGtJXXdbiph7ITaOpC8mW-rZM2PG0","auth":"iou2_bZhrxy8MOmy6wu3Rw"}};
 push.sendNotification(sub,'test message')
